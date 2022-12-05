2dd1ed869d143814e7ab68a4840be52de05eed2d
markymelnik <markemelnik@gmail.com>
2022-12-05 12:59:17 -0500

    Gameboard objects generates a gameboard (nested array) and assigns a choice (X or O) to its index

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ const Playerselection = (() => {
     let playerTwo = playerFactory('Kareem', 'O');
     
 })();
+
+const Gameboard = (() => {
+
+    let gameboard = [['', '', ''], ['', '', ''], ['', '', '']];
+
+    const newBoard = () => [...gameboard];
+
+    const setTiles = (index, choice) => {
+        for (let i = 0; i < gameboard.length; i++) {
+            for (let j = 0; j < gameboard[i].length; j++) {
+                gameboard[i][j][index] = choice;
+                console.log({index, choice})
+            }
+            console.log("Test");
+        }
+    }
+
+    return {
+        newBoard,
+        setTiles
+    }
+
+})();
